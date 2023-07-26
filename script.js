736189c410b1a301f145136defa364e6093928c9
Jugal Shrestha <jugalnepali@gmail.com>
2023-07-26 17:38:31 +0545

    Remaked Bubble sort And Retouched the UI

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //All the doms
 
-const arrayGenerator = document.querySelector('.array-generator')
-const arraySpeed = document.querySelector('.array-speed-controller')
-const arraySize = document.querySelector('.array-size-controller')
-const sortingAlgorithms = document.querySelectorAll('.sorting-algo')
-const sorter = document.querySelector('.sorter')
+const arrayGenerator = document.querySelector('.array-generator');
+const arraySpeed = document.querySelector('.array-speed-controller');
+const arraySize = document.querySelector('.array-size-controller');
+const sortingAlgorithms = document.querySelectorAll('.sorting-algo');
+const sorter = document.querySelector('.sorter');
 
-const visualizer = document.querySelector('.visualizer')
+const visualizer = document.querySelector('.visualizer');
+const swapDisplay = document.querySelector('.swap-display .text');
+const infoAboutAlgo = document.querySelector(".info-about-algo");
 
-const blocker = document.querySelector('.blocker')
-
-const loader = document.querySelector('.loader')
-const everything = document.querySelector('.everything')
+const loader = document.querySelector('.loader');
+const everything = document.querySelector('.everything');
 
 //--------------Loading screen--------------------
 
@@ -43,7 +43,10 @@ function deleteArray() {
 
 //New array generator
 function generateArray(){
-    deleteArray()
+    deleteArray();
+    sorter.style.pointerEvents = "all";
+    sorter.innerText = "sort";
+    swapDisplay.innerText = "...";
     var max = 50;
     var min = 1;
     var size = arraySize.value || Math.floor(Math.random()*(15-4+1)+4)
@@ -64,6 +67,9 @@ function generateArray(){
 //to generate array on load
 generateArray()
 
+//to generate info on load
+infoAboutAlgo.innerHTML = "<li>The simplest sorting algorithm.<li>Repeatedly compares adjacent elements and swaps them if they are in the wrong order.<li>The largest elements bubble up to their correct positions in each pass.<li>Time complexity: O(n^2) in the worst and average case, O(n) in the best case (when the array is already sorted)."
+
 arrayGenerator.addEventListener('click',generateArray)
 
 //--------------Algo Selector---------------------------
@@ -73,13 +79,13 @@ let algo = 'bubble'
 sortingAlgorithms.forEach(algorithm=>{
     algorithm.addEventListener('click',()=>{
         sortingAlgorithms.forEach(algorithm=>{    
-            algorithm.style.background = "var(--s-color)"
-            algorithm.style.boxShadow = 'none'
-            algorithm.style.fontWeight = '300'
+            algorithm.style.background = "var(--p-color)"
+            algorithm.style.color = "var(--s-color)";
+            algorithm.style.boxShadow = '.5vh .5vh var(--n1-color)'
         })
         algo = algorithm.innerText.toLowerCase()
         algorithm.style.background = "var(--n1-color)"
-        algorithm.style.fontWeight = '900'
+        algorithm.style.color = "var(--p-color)";
         algorithm.style.boxShadow = '.5vh .5vh var(--shadow)'
     })
 })
@@ -98,9 +104,8 @@ function swap(array,i,j)
 }
 
 async function sort(option){
-    blocker.style.display = "block"
     var size = visualizer.children.length
-    var speed = arraySpeed.value*1000 || 1000
+    var speed = arraySpeed.value*800 || 800
     //Bubble sort
     switch(option)
     {
@@ -109,23 +114,38 @@ async function sort(option){
             {
                 for(var j=0;j<size-i-1;j++)
                 {
-                    await sleep(speed)
                     if(array[j]>array[j+1])
                     {
-                        visualizer.children[j].classList.add('sorting-1')
-                        visualizer.children[j+1].classList.add('sorting-2')
-                        swap(array,j,j+1)
+                        visualizer.children[j].classList.add('sorting-2');
+                        visualizer.children[j+1].classList.add('sorting-1');
+                        swapDisplay.innerText = array[j] + " > "+ array[j+1] + " ,So moving "+ array[j];
+                        swap(array,j,j+1);
                         visualizer.children[j].innerText = array[j]
                         visualizer.children[j+1].innerText = array[j+1]
-                        await sleep(speed-500)
-                        visualizer.children[j].classList.remove('sorting-1')
-                        visualizer.children[j+1].classList.remove('sorting-2')
+                        await sleep(speed)
+                        swapDisplay.innerText = "...";
+                        visualizer.children[j].classList.remove('sorting-2')
+                        visualizer.children[j+1].classList.remove('sorting-1')
+                    }
+                    else{
+                        visualizer.children[j].classList.add('sorting-2');
+                        visualizer.children[j+1].classList.add('sorting-1');
+                        swapDisplay.innerText = array[j] + " < "+ array[j+1] + " ,So selected "+array[j+1];
+                        await sleep(speed);
+                        swapDisplay.innerText = "...";
+                        visualizer.children[j].classList.remove('sorting-2');
+                        visualizer.children[j+1].classList.remove('sorting-1');
+                        
                     }
                 }
-                visualizer.children[size-i-1].classList.add('sorted')
+                swapDisplay.innerText = "Sorted "+array[size-i-1];
+                visualizer.children[size-i-1].classList.add('sorted');
+                await sleep(speed);
             }
-            visualizer.firstChild.classList.add('sorted')
-            blocker.style.display = "none"
+            swapDisplay.innerText = "Sorted!";
+            visualizer.firstChild.classList.add('sorted');
+            sorter.style.pointerEvents = "all";
+            sorter.innerText = "sort";
             break;
 
         case 'selection':
@@ -152,16 +172,13 @@ async function sort(option){
                 visualizer.children[i].classList.add('sorted')
             }
             visualizer.lastChild.classList.add('sorted')
-            blocker.style.display = "none"
             break;
 
         case 'merge':
             console.log('merge sort');
-            blocker.style.display = "none"
             break;
         case 'quick':
             console.log('quick sort');
-            blocker.style.display = "none"
             break;
         case 'insertion': 
             for (i = 1; i < size; i++)
@@ -188,16 +205,16 @@ async function sort(option){
             {
                 visualizer.children[i].classList.add('sorted')
             }
-            blocker.style.display = "none"
             break;
 
         default:
             console.log('default');
-            blocker.style.display = "none"
             break;
     }
 }
 
 sorter.addEventListener('click',()=>{
     sort(algo)
+    sorter.style.pointerEvents = "none";
+    sorter.innerText = "sorting!";
 })
